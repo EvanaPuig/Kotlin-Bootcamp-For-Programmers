4ba7580f901f316a28a8e8c393761051fb298a10
Evana Margain Puig <evanamp233@gmail.com>
2020-12-19 17:59:20 -0600

    Practicing interface delegation and abstract classes with spices

--- src/main/kotlin/practice/spices/Spice.kt
+++ src/main/kotlin/practice/spices/Spice.kt
@@ -1,22 +1,17 @@
 package practice.spices
 
 fun main() {
-    val spices = mutableListOf(
-        Spice("curry", "mild"),
-        Spice("pepper", "hot"),
-        Spice("chilli", "very hot")
-    )
-
-    spices.add(makeSalt())
+    val spices = mutableListOf<Spice>()
 
     spices.filter {
         it.heat <= 5
     }
+
+    var mildCurry = Curry("mild")
+    println(mildCurry.color)
 }
 
-fun makeSalt() = Spice("salt", "none")
-
-class Spice(val name: String, val spiciness: String = "mild") {
+abstract class Spice(val name: String, val spiciness: String = "mild", color: SpiceColor): SpiceColor by color {
     val heat: Int
         get() = when(spiciness)  {
             "hot" -> 10
@@ -28,5 +23,30 @@ class Spice(val name: String, val spiciness: String = "mild") {
         println("The spice: $name is in hot scale $heat")
     }
 
+    abstract fun prepareSpice()
+}
 
+class Curry(spiciness: String, color: SpiceColor = YellowColor):
+    Spice("curry", spiciness = spiciness, color),
+    Grinder {
+        override fun prepareSpice() {
+            grind()
+        }
+
+        override fun grind() {
+
+        }
+
+}
+
+interface Grinder {
+    fun grind()
+}
+
+interface SpiceColor {
+    val color: String
+}
+
+object YellowColor : SpiceColor {
+    override val color = "yellow"
 }

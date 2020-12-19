81a3a311e3c3ad796bb1f28863957b7d91b3e3c5
Evana Margain Puig <evanamp233@gmail.com>
2020-12-19 12:50:23 -0600

    Creates the can add more fish function and a new file for activity suggestion

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -1,8 +1,12 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    println("Hello ${args[0]}!")
-    feedTheFish()
+    // println("Hello ${args[0]}!")
+    // feedTheFish()
+    println(canAddFish(10.0, listOf(3,3,3)))
+    println(canAddFish(8.0, listOf(2,2,2), hasDecorations = false))
+    println(canAddFish(9.0, listOf(1,1,3), 3))
+    println(canAddFish(10.0, listOf(), 7, true))
 }
 
 fun feedTheFish() {
@@ -39,4 +43,13 @@ fun dayOfWeek() {
         6 -> println("Friday")
         7 -> println("Saturday")
     }
+}
+
+fun canAddFish(
+    tankSize: Double,
+    currentFish: List<Int>,
+    fishSize: Int = 2,
+    hasDecorations: Boolean = true
+): Boolean {
+    return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 }

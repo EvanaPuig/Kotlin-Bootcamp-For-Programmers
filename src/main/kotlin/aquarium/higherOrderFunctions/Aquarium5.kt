93eb75938c438370cc687d121edb2d4827d0e1b4
Evana Margain Puig <evanamp233@gmail.com>
2020-12-26 16:31:40 -0600

    Adds the practice of higher order functions

--- src/main/kotlin/aquarium/higherOrderFunctions/Aquarium5.kt
+++ src/main/kotlin/aquarium/higherOrderFunctions/Aquarium5.kt
@@ -9,9 +9,23 @@ fun main() {
 fun fishExamples() {
     val fish  = Fish("splashy")
 
+    // without inline an object is created every call to myWith
     myWith(fish.name) {
-        println(capitalize())
+        capitalize()
     }
+
+    /*
+    This is the equivalent of what the code without inline is doing
+    myWith(fish.name, object : Function1<String, Unit>  {
+        override fun invoke(name: String) {
+            name.capitalize()
+        }
+    })
+     */
+
+    // with inline no object is created, and lambda body is inlined here
+    // fish.name.capitalize()
+    // inline in complex functions does increase the code size
 
     // run returns the result of executing the lambda
     println(fish.run { name })
@@ -26,6 +40,6 @@ fun fishExamples() {
             .let { it + 31 })
 }
 
-fun myWith(name: String, block: String.() -> Unit) {
+inline fun myWith(name: String, block: String.() -> Unit) {
     name.block()
 }
--- src/main/kotlin/practice/higherOrderFunctionsGame/higherOrderPractice.kt
+++ src/main/kotlin/practice/higherOrderFunctionsGame/higherOrderPractice.kt
@@ -1,2 +1,18 @@
 package practice.higherOrderFunctionsGame
 
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+    println(numbers.numbersDivisibleBy3 {
+        it.rem(3)
+    })
+}
+
+fun List<Int>.numbersDivisibleBy3(block: (Int) -> Int): List<Int> {
+    val result = mutableListOf<Int>()
+    for (item in this) {
+        if (block(item) == 0) {
+            result.add(item)
+        }
+    }
+    return result
+}

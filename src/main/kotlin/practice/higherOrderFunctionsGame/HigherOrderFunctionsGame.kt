85496a8c2c003f8b3a0489672b0c8c14870f6cca
Evana Margain Puig <evanamp233@gmail.com>
2020-12-26 14:38:13 -0600

    Adds the game sample starter code

---
+++ src/main/kotlin/practice/higherOrderFunctionsGame/HigherOrderFunctionsGame.kt
@@ -0,0 +1,45 @@
+package practice.higherOrderFunctionsGame
+
+fun main() {
+    val game = Game()
+    println(game.path)
+    game.north()
+    game.east()
+    game.south()
+    game.west()
+    game.end()
+    println(game.path)
+}
+
+enum class Direction {
+    NORTH, SOUTH, EAST, WEST, START, END
+}
+
+class Game {
+    var path = mutableListOf(Direction.START)
+
+    val north = {
+        path.add(Direction.NORTH)
+    }
+
+    val south = {
+        path.add(Direction.SOUTH)
+    }
+
+    val east = {
+        path.add(Direction.EAST)
+    }
+
+    val west = {
+        path.add(Direction.WEST)
+    }
+
+    val end = {
+        path.add(Direction.END)
+        println("Game Over")
+        println(path)
+        path.clear()
+        false
+    }
+}
+
